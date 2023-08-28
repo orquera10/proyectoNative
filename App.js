35dcc36106ed714f46c7dee2db5623bed924b0bd
Dario <orquera10@gmail.com>
2023-08-28 20:52:07 -0300

    cambio en app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hola mundo!</Text>
+      <Text>hola Coder!</Text>
       <StatusBar style="auto" />
     </View>
   );
